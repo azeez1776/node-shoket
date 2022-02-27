e3ba0d77722950b547d87d309fc5372afe654b57
azeez1776 <aboud3am81@gmail.com>
2022-02-27 21:37:42 +0300

    used the fetch function in the makePayment function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-const {fetch} = require('node-fetch');
+const fetch = require('node-fetch');
+
+const makePayment = async(api_key, amount, customer_name, email, number_used, channel) => {
+    try{
+        const response = await fetch('https://api.shoket.co/v1/charge/',{
+            method:'post',
+            body:JSON.stringify({
+                amount:amount,
+                customer_name:customer_name,
+                email:email,
+                number_used:number_used,
+                channel:channel
+            })
+        })
+    }
+}
