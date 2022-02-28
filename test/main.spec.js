1501c1d3929a843d7d3508dbe97a0ff856cca5b5
azeez1776 <aboud3am81@gmail.com>
2022-03-01 01:34:06 +0300

    imported rest and setupServer from msw

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,5 +1,7 @@
 const {makePayment} = require('../src/index');
 const {isFunction} = require('lodash');
+const {rest} = require('msw');
+const {setupServer} = require('msw/node');
 // import {makePayment} from '../src/index';
 // import {isFunction} from 'lodash';
 
