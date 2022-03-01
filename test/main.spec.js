ad21914e9dfdc3ab7774da5cd4172949b839cfbd
azeez1776 <aboud3am81@gmail.com>
2022-03-01 09:46:18 +0300

    tested verifyPayment if function

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,10 +1,16 @@
-const {makePayment} = require('../src/index');
+const {makePayment, verifyPayment} = require('../src/index');
 const {isFunction} = require('lodash');
 
 
 describe('testing the makePayment function', () => {
     test('testing if is a function', () => {
         expect(isFunction(makePayment)).toBe(true);
+    })
+});
+
+describe('testing the verifyPayment', () => {
+    test('testing if is a function', () => {
+        expect(isFunction(verifyPayment)).toBe(true);
     })
 })
 
